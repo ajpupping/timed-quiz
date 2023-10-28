00dbdb0b4d2adff2c4075f853cf56ed028cd265d
Alyssa <125580811+ajpupping@users.noreply.github.com>
2023-10-28 12:08:47 -0700

    started html layout, added to do list and notes in js

---
+++ script.js
@@ -0,0 +1,44 @@
+// To Do List
+
+// create questions array
+// object with question and then options array and answer property
+
+//{ example
+//  question: what is the best way to listen to music?
+//  options: [spotify, apple music, physical media, radio]
+//  answer: apple music
+//}
+
+//currentQuestion = 0
+
+// questions[currentQuestion].question, questions[currentQuestion].answer
+
+// create question element
+// append option elements
+// event handler => need to check if clicked option is answer
+// does target value === answer
+
+// where are we in our question list
+// need to track where with the index
+
+// no jquery or bootstrap for this assignment 
+
+// does not matter if it is fair
+// time left is the score 
+// wrong answer time penalty should make it worth it to answer correctly
+
+// need a function for starting the timer 
+// need a function for starting the questions 
+// both those get called on quiz start 
+
+// at the end of quiz save time score and display high scores 
+// need to ask for user input to name high score 
+// local storage
+
+// first focus on showing one question and then the next question 
+// do not need to shuffle questions 
+// does not need to be mobile responsive 
+// just clean and simple design 
+
+// easiest way to show high scores is to link a second html page
+// see slack note 

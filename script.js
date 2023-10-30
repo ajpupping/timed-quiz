24d2c08b82001e6099ce91bb3887a3e6deeb4d0e
Alyssa <125580811+ajpupping@users.noreply.github.com>
2023-10-29 21:02:49 -0700

    fixed hidden and shown elements, added play again function, additional css styling

--- script.js
+++ script.js
@@ -76,6 +76,7 @@ var timeLeft;
 var timeInterval;
 var index = 0
 var answerEl = document.getElementById('answerContainer')
+var playAgainEl = document.getElementById('playAgainButton')
 
 // displays questions one at a time, checks if answers are correct
 function startQuiz() {
@@ -86,9 +87,10 @@ function startQuiz() {
 }
 
 function renderQuiz() {
+    document.getElementById('showQuestions').classList.add('show')
     document.getElementById('questionTitle').textContent = questions[index].question
     answerEl.innerHTML = ''
-    questions[index].choices.forEach(function(choice){
+    questions[index].choices.forEach(function (choice) {
         var btn = document.createElement('button')
         btn.className = 'button'
         btn.textContent = choice
@@ -98,17 +100,16 @@ function renderQuiz() {
 }
 
 function checkAnswer(event) {
-    console.log('clicked')
-    if(event.target.textContent !== questions[index].answer) {
+    if (event.target.textContent !== questions[index].answer) {
         timeLeft -= 10
-        if(timeLeft <= 0) {
+        if (timeLeft <= 0) {
             timeLeft = 0
             timeEl.textContent = timeLeft
             endQuiz()
         }
     } // else, add feedback
-    index++ 
-    if(index===questions.length){
+    index++
+    if (index === questions.length) {
         endQuiz()
     } else {
         renderQuiz()
@@ -117,20 +118,25 @@ function checkAnswer(event) {
 
 // starts timer
 function startTimer() {
-    timeInterval = setInterval(function() {
+    timeInterval = setInterval(function () {
         timeLeft--
         timeEl.textContent = timeLeft
-        if(timeLeft <= 0) {
+        if (timeLeft <= 0) {
             endQuiz()
         }
     }, 1000)
-    
 }
 
 function endQuiz() {
     clearInterval(timeInterval)
     document.getElementById('questionContainer').classList.add('hide')
+    document.getElementById('showScores').classList.add('show')
+}
+
+function playAgain() {
+    location.reload();
 }
 
 startButton.addEventListener('click', startQuiz)
 startButton.addEventListener('click', startTimer)
+playAgainEl.addEventListener('click', playAgain)
